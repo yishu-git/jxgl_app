a64a70c3e99600bd9587ece5b112eed92795597d
zhaijinsong <1633825587@qq.com>
2020-12-14 10:41:15 +0800

    增加接口

--- src/public/httpApi.js
+++ src/public/httpApi.js
@@ -1,8 +1,9 @@
 import {
-  myget,
-  mypost
+    myget,
+    mypost
 } from './axios.js'
-export const getMachineryList=async ()=>await myget('/jixie')
-export const getNavigate=async ()=>await myget('/navigate-tree')
-export const getcategory=async ()=>await mypost('/category')
-export const getnames=async (data)=>await mypost('/names',data)
+export const getMachineryList = async() => await myget('/jixie')
+export const getNavigate = async() => await myget('/navigate-tree')
+export const getcategory = async() => await mypost('/category')
+export const getnames2 = async(data) => await mypost('/names', data)
+export const getnames = async(data) => await mypost('/names', data)
